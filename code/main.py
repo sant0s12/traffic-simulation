85db99251479680b4be48077572d128c54dba400
sant0s12 <diego.delossantos12@gmail.com>
2020-11-16 10:51:50 +0100

    diver speed distribution

--- code/main.py
+++ code/main.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 import random
+import numpy as np
 pygame.init()
 
 BLACK = (0, 0, 0)
@@ -20,22 +21,26 @@ class Car(pygame.sprite.Sprite):
         startpos: starting position in screen coords
         direction: direction of the car (for our purposes only straight lines)
     """
-    def __init__(self, screen, road, startpos, direction):
+    def __init__(self, screen, road, startpos, speed: float):
         super(Car, self).__init__()
         self.surface = pygame.Surface((10, 10))
         self.surface.fill(WHITE)
         self.rect = self.surface.get_rect()
+
+        self.pos = list(startpos)
         self.rect.center = startpos
+
         self.screen = screen
         self.road = road
-        self.direction = direction
+        self.speed = speed
 
     def update(self):
         """
         Move object along direction vector
         """
 
-        self.rect.move_ip(self.direction)
+        self.pos[0] += self.speed
+        self.rect.center = self.pos # hself.direction)
 
     def draw(self):
         """
@@ -55,7 +60,7 @@ class Road:
         frequency: (more like deltaT at the moment) car creation frequency
     """
 
-    def __init__(self, screen, position, lanes=2, lanewidth=5, frequency=60):
+    def __init__(self, screen, position, lanes=2, lanewidth=5, frequency=60, avg_speed=100, speed_sigma=10):
         self.screen = screen
         self.carlist = []
         self.frequency = frequency
@@ -63,6 +68,8 @@ class Road:
         self.position = position
         self.lanes = lanes
         self.lanewidth = lanewidth
+        self.avg_speed = avg_speed
+        self.speed_sigma = speed_sigma
 
     def update(self):
         """
@@ -73,7 +80,9 @@ class Road:
         if self.ticks >= self.frequency:
             self.ticks = 0
             lane = random.choice(range(self.lanes)) * self.lanewidth
-            newCar = Car(self.screen, self, (self.position[0], self.position[1] + lane), (1,0))
+            v = np.random.normal(self.avg_speed, self.speed_sigma)/100.0
+            print(v)
+            newCar = Car(self.screen, self, (self.position[0], self.position[1] + lane), v)
             self.carlist.append(newCar)
 
         for car in self.carlist:
@@ -85,7 +94,7 @@ class Road:
                 car.draw()
 
 if __name__ == "__main__":
-    road = Road(screen, (0, height/2), lanewidth=20, frequency=30, lanes=5)
+    road = Road(screen, (0, height/2), lanewidth=20, frequency=30, lanes=1)
 
     # Main game loop
     while 1:
