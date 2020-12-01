e1de7d9d762b42c65d73bbca90001bb4a31227b1
vvitez <66899393+vvitez@users.noreply.github.com>
2020-12-01 03:39:29 +0100

    Update main.py

--- code/main.py
+++ code/main.py
@@ -18,7 +18,7 @@ def dist(a, b):
     return np.sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2)
 
 if __name__ == "__main__":
-    a = ModelParams(v_0=30, s_0=2, s_1=0, T=1.6, a=2, b=1.67, delta=4, length=5, thr=0.4, pol=0.5)
+    a = ModelParams(v_0=30, s_0=2, s_1=0, T=1.6, a=2, b=1.67, delta=4, length=5, thr=0.4, pol=0.5, fs=5, fp=0.01)
 	# v_0 = 125 and 80 km/h, rest are standard values from the paper/traffic-simulation.de
     car_normal = ModelParams(v_0=34.72, T=1.5, a=0.3, b=3, delta=4, s_0=2, s_1=0, length=5, thr=0.2, pol=0.5)
     truck_normal = ModelParams(v_0=22.224, T=1.7, a=0.3, b=2, delta=4, s_0=2, s_1=0, length=16.5, thr=0.2, pol=0.5)
