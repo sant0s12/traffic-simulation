b8e7cb5c8352fa36f9e6647541d466889729b4e2
sant0s12 <diego.delossantos12@gmail.com>
2020-12-03 12:08:07 +0100

    make image out of the dot thing and save it

--- code/main.py
+++ code/main.py
@@ -1,6 +1,5 @@
 import sys
 import pygame
-import matplotlib.pyplot as plt
 import pickle
 import os.path
 from Simulation import Simulation
@@ -22,29 +21,44 @@ CLOCK = pygame.time.Clock()
 def dist(a, b):
     return np.sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2)
 
-def save_data(data, filename, overwrite=False):
-    def make_filename(f):
-        suff = ""
-        i = 1
-        while os.path.isfile(f'{f}{suff}'): suff = i ; i += 1
-        return f'{f}{suff}'
+def make_filename(f):
+    fsplit = f.rsplit(".", 1)
+    name = fsplit[0]
+    ext = fsplit[1] if len(fsplit) > 1 else ""
 
-    pickle.dump(data, open(make_filename(filename), "wb"))
+    suff = ""
+    i = 1
+    while os.path.isfile(f'{name}{suff}.{ext}'): suff = i ; i += 1
+    return f'{name}{suff}.{ext}'
+
+def save_data(data, filename, overwrite=False):
+    pickle.dump(data, open(make_filename(filename) if not overwrite else filename, "wb"))
 
 def read_data(filename):
     if not os.path.isfile(filename): return None
-    else: return picke.load(open(filename, "rb"))
+    else: return pickle.load(open(filename, "rb"))
+
+def dots_to_image(pixel_plot, filename, overwrite=False):
+    from PIL import Image
+    filename = (make_filename(filename) if not overwrite else filename)
+
+    img = Image.fromarray(pixel_plot.astype('uint8') * 255, mode='L')
+    img.save(filename)
 
 if __name__ == "__main__":
-    road_length = 5000
-    a = Params(v_0=30, s_0=2, s_1=0, T=1.6, a=2, b=1.67, delta=4, length=5, thr=0.4, pol=0.5)
-    sim = Simulation(params_list=[a], delta_t=DELTA_T, car_frequency=1.5, road_length=road_length)
-    data = sim.run(50)
-    save_data(data, "agsh")
+    road_length = 500000
+    a = Params(v_0=(30, 1), s_0=2, s_1=0, T=1.6, a=2, b=1.67, delta=4, length=5, thr=0.4, pol=0.5)
+    sim = Simulation(params_list=[a], delta_t=DELTA_T, car_frequency=1.5, road_length=road_length, road_lanes=1)
 
-    imdata = Metrics.plot_dots(data, road_length, 1, 5)
-    plt.imshow(imdata, cmap='gray')
-    plt.show()
+    filename = "testing.p"
+    data = read_data(filename)
+    if data is None:
+        data = sim.run(1000)
+        save_data(data, filename)
+
+    dotgraph = Metrics.make_dots(data, road_length, 1, 5)
+    dots_to_image(dotgraph, "dot_image.png")
+    # Metrics.show_dots(dotgraph)
 
 """    pygame.init()
 
