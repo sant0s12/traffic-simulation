6bc78c624935f916f4472df880c34daeb33b4043
sant0s12 <diego.delossantos12@gmail.com>
2020-12-03 10:40:43 +0100

    saving car data

--- code/main.py
+++ code/main.py
@@ -1,9 +1,12 @@
 import sys
 import pygame
+import matplotlib.pyplot as plt
+import pickle
+import os.path
 from Simulation import Simulation
 from DriverModel import Driver
-import Car
-from Road import Road
+from Metrics import Metrics
+from Car import Params
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -12,22 +15,38 @@ DELTA_T = 0.1 # delta_t in seconds
 SPEED = 5 # playback speed of the data (1 for real time)
 
 SIZE = WIDTH, HEIGHT = 1000, 100
-SCREEN = pygame.display.set_mode(SIZE)
+#SCREEN = pygame.display.set_mode(SIZE)
 
 CLOCK = pygame.time.Clock()
 
 def dist(a, b):
     return np.sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2)
 
+def save_data(data, filename, overwrite=False):
+    def make_filename(f):
+        suff = ""
+        i = 1
+        while os.path.isfile(f'{f}{suff}'): suff = i ; i += 1
+        return f'{f}{suff}'
+
+    pickle.dump(data, open(make_filename(filename), "wb"))
+
+def read_data(filename):
+    if not os.path.isfile(filename): return None
+    else: return picke.load(open(filename, "rb"))
+
 if __name__ == "__main__":
-    #v_0 = 125 and 80 km/h, rest are standard values from the paper/traffic-simulation.de
-    car_normal = Car.Params(v_0=(34.72, 3), T=1.5, a=0.3, b=3, delta=4, s_0=2, s_1=0, length=5, thr=0.2, pol=0.5, spawn_weight=126090, fail_p=0.0001, fail_steps=1)
-    truck_normal = Car.Params(v_0=22.224, T=1.7, a=0.3, b=2, delta=4, s_0=2, s_1=0, length=16.5, thr=0.2, pol=0.5, spawn_weight=6730)
+    road_length = 5000
+    a = Params(v_0=30, s_0=2, s_1=0, T=1.6, a=2, b=1.67, delta=4, length=5, thr=0.4, pol=0.5)
+    sim = Simulation(params_list=[a], delta_t=DELTA_T, car_frequency=1.5, road_length=road_length)
+    data = sim.run(50)
+    save_data(data, "agsh")
 
-    sim = Simulation(params_list=[car_normal, truck_normal], delta_t=DELTA_T, car_frequency=1/1.6)
-    data = sim.run(250)
+    imdata = Metrics.plot_dots(data, road_length, 1, 5)
+    plt.imshow(imdata, cmap='gray')
+    plt.show()
 
-    pygame.init()
+"""    pygame.init()
 
     for datapoint in data:
         for event in pygame.event.get():
@@ -35,11 +54,8 @@ if __name__ == "__main__":
 
         SCREEN.fill(BLACK)
 
-        avg_sum = 0.
         for car in datapoint:
             SCREEN.blit(car['surface'], car['rect'])
-            avg_sum += car['v'] * 3.6
-        print(avg_sum/len(datapoint))
 
         pygame.display.update()
-        CLOCK.tick(1./DELTA_T * SPEED)
+        CLOCK.tick(1./DELTA_T * SPEED) """
