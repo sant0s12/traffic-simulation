06cd64dd4fc16a67ce1666922091362503f4364a
sant0s12 <diego.delossantos12@gmail.com>
2020-12-11 12:05:03 +0100

    no speedlimit sim and folders

--- code/main.py
+++ code/main.py
@@ -25,6 +25,7 @@ def make_filename(f):
 
 def save_data(data, filename, overwrite=False):
     print("Saving data, this might take a while...")
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
     json.dump(data, open(make_filename(filename) if not overwrite else filename, "w"))
 
 def read_data(filename):
@@ -37,6 +38,7 @@ def dots_to_image(pixel_plot, filename, mode='L', overwrite=False):
     from PIL import Image
     filename = (make_filename(filename) if not overwrite else filename)
 
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
     img = Image.fromarray(pixel_plot.astype('uint8'), mode=mode)
     img.save(filename)
 
@@ -101,9 +103,10 @@ if __name__ == "__main__":
 
     for p in fail_ps:
         for l in limits:
-            filename_data=f'speedlimit_{l}_fail_{p}.json'
-            filename_average=f'speedlimit_{l}_fail_{p}_average.json'
-            filename_graph=f'speedlimit_{l}_fail_{p}_graph.png'
+            folder='speedlimit/'
+            filename_data=folder + f'speedlimit_{l}_fail_{p}.json'
+            filename_average=folder + f'speedlimit_{l}_fail_{p}_average.json'
+            filename_graph=folder + f'speedlimit_{l}_fail_{p}_graph.png'
             data = read_data(filename=filename_data)
             if data is None:
                 print(f'Running simulation for speedlimit={l}, fail_p={p}')
@@ -126,17 +129,46 @@ if __name__ == "__main__":
 
     #===========================================================================================
 
-    #show_pygame(data_list[3], delta_t)
-    
-    #avg = []
-    #med = []
-    #for data in data_list:
-    #    avg.append(np.average(Metrics.avg_speed(data)))
-    #r   med.append(np.median(Metrics.median_speed(data)))
 
+    # With no speed limit and constant params
+    #===========================================================================================
+    car_params = Params(T=1.4, a=2, b=2.5, delta=4, s_0=2, s_1=0, length=4.55,
+                        thr=0.3, pol=0.25, right_bias=0.3, fail_steps=900,
+                        spawn_weight=139829)
 
-    #plt.plot(limits, avg, marker='o')
-    #plt.plot(limits, med, marker='o')
-    #plt.bar(limits, avg)
-    #plt.bar(limits, med)
-    #plt.show()
+    trucc_params = Params(T=1.6, a=1, b=1.5, delta=4, s_0=2, s_1=0, length=16.5,
+                        thr=0.3, pol=0.25, right_bias=0.3, fail_steps=900,
+                        spawn_weight=7886)
+
+    avgs = [100, 120, 140, 160, 180]
+    widths = [10, 15, 20]
+    fail_ps = [0, 1e-6]
+
+    for p in fail_ps:
+        for a in avgs:
+            for w in widths:
+                folder='no_speedlimit/'
+                filename_data=folder + f'no_speedlimit_({a}, {w})_fail_{p}.json'
+                filename_average=folder + f'no_speedlimit_({a}, {w})_fail_{p}_average.json'
+                filename_graph=folder + f'no_speedlimit_({a}, {w})_fail_{p}.png'
+                data = read_data(filename=filename_data)
+                if data is None:
+                    print(f'Running simulation for average speed={a}, width={w}, fail_p={p}')
+
+                    car_params.v_0 = (a/3.6, w/3.6)
+                    car_params.fail_p = p
+
+                    trucc_params.v_0 = (min(a, 80)/3.6, 2.5/3.6)
+                    trucc_params.fail_p = p
+
+                    sim = Simulation([car_params, trucc_params], road_length=road_length, car_frequency=2, delta_t=delta_t)
+                    data = sim.run()
+                    save_data(data, filename_data, overwrite=True)
+
+                avg = Metrics.avg_speed(data)
+                save_data(avg, filename_average, overwrite=True)
+
+                dots = Metrics.make_dots_bw(data, road_length, time_div=1, delta_x=10)
+                dots_to_image(dots, filename_graph, overwrite=True)
+
+    #===========================================================================================
