9cf7f5d95642ec061d14bb84676c83a7a0de0a3b
sant0s12 <diego.delossantos12@gmail.com>
2020-12-11 19:36:58 +0100

    a=0.3

--- code/main.py
+++ code/main.py
@@ -209,3 +209,64 @@ if __name__ == "__main__":
 
         Metrics.plot_bar_groupped(avg_table, filename_plot_groupped, widths, xticks=avgs, ylabel="Average speed", xlabel="Desired speed")
     #===========================================================================================
+
+    # With no speed limit and a=0.3
+    #===========================================================================================
+    car_params = Params(T=1.4, a=0.3, b=2.5, delta=4, s_0=2, s_1=0, length=4.55,
+                        thr=0.3, pol=0.25, right_bias=0.3, fail_steps=900,
+                        spawn_weight=139829)
+
+    trucc_params = Params(T=1.6, a=0.3, b=1.5, delta=4, s_0=2, s_1=0, length=16.5,
+                        thr=0.3, pol=0.25, right_bias=0.3, fail_steps=900,
+                        spawn_weight=7886)
+
+    avgs = [100, 120, 140, 160, 180]
+    widths = [10, 15, 20]
+    fail_ps = [0, 1e-06]
+    avg_table = np.zeros((len(widths), len(avgs)))
+
+    for p in fail_ps:
+        folder=f'no_speedlimit_a_0,3/{p}/'
+        filename_plot_groupped=folder + f'no_speedlimit_graph_groupped_a_0,3.svg'
+        for i, w in enumerate(widths):
+            filename_plot=folder + f'no_speedlimit_plot_width_{w}_a_0,3.svg'
+            for j, a in enumerate(avgs):
+                filename_data=folder + f'no_speedlimit_({a}, {w})_fail_{p}_a_0,3.json'
+                filename_average=folder + f'no_speedlimit_({a}, {w})_fail_{p}_average_a_0,3.json'
+                filename_graph=folder + f'no_speedlimit_({a}, {w})_fail_{p}_a_0,3.png'
+                filename_average_plot=folder + f'no_speedlimit_{a}_width_{w}_average_plot_a_0,3.svg'
+
+                data = read_data(filename=filename_data)
+                if data is None:
+                    print(f'Running simulation for average speed={a}, width={w}, fail_p={p}')
+
+                    car_params.v_0 = (a/3.6, w/3.6)
+                    car_params.fail_p = p
+
+                    trucc_params.v_0 = (min(a, 80)/3.6, 2.5/3.6)
+                    trucc_params.fail_p = p
+
+                    sim = Simulation([car_params, trucc_params], road_length=road_length, car_frequency=2, delta_t=delta_t)
+                    data = sim.run()
+                    save_data(data, filename_data, overwrite=True)
+
+                avg = read_data(filename_average)
+                if avg is None:
+                    avg = Metrics.avg_speed(data)
+                    save_data(avg, filename_average, overwrite=True)
+                avg_table[i][j] = np.mean(avg)
+
+                Metrics.plot_bins(100, avg, filename_average_plot)
+
+                dots = Metrics.make_dots_bw(data, road_length, time_div=1, delta_x=10)
+                dots_to_image(dots, filename_graph, overwrite=True)
+
+            plt.bar(range(len(avgs)), avg_table[i])
+            plt.xticks(range(len(avgs)), avgs)
+            plt.ylabel("Average speed")
+            plt.xlabel("Average desired speed")
+            plt.savefig(filename_plot)
+            plt.close()
+
+        Metrics.plot_bar_groupped(avg_table, filename_plot_groupped, widths, xticks=avgs, ylabel="Average speed", xlabel="Desired speed")
+    #===========================================================================================
